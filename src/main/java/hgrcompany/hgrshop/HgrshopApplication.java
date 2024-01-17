b4139268ef5c6cad4914e6972b91290263ccd84b
MTsauRus <qksrurwnsql@naver.com>
2024-01-17 22:19:16 +0900

    first commit

---
+++ src/main/java/hgrcompany/hgrshop/HgrshopApplication.java
@@ -0,0 +1,13 @@
+package hgrcompany.hgrshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HgrshopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HgrshopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/hgrcompany/hgrshop/HgrshopApplicationTests.java
@@ -0,0 +1,13 @@
+package hgrcompany.hgrshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HgrshopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
